bdf3f429995b3782bb37ee63577976ee1f2f3157
Konpaku Youmu <saber2pr@gmail.com>
2020-05-20 15:31:00 +0800

    Update component.js

--- component.js
+++ component.js
@@ -1,11 +1,18 @@
 (() => {
-  document
-    .querySelectorAll('template')
-    .forEach(tpl => customElements.define(tpl.id, class extends HTMLElement {
-      constructor() {
-        super()
-        const shadow = this.attachShadow({ mode: 'open' })
-        shadow.innerHTML = tpl.innerHTML
+  const Mustache = /\{\{(.*?)\}\}/g
+  const tpls = document.querySelectorAll('template')
+  tpls.forEach(tpl => customElements.define(tpl.id, class extends HTMLElement {
+    constructor() {
+      super()
+      const shadow = this.attachShadow({ mode: 'open' })
+      const describe = tpl.innerHTML
+      const mapPropToAttr = (_, prop) => {
+        const key = prop.trim()
+        const attr = this.getAttribute(key)
+        this.removeAttribute(key)
+        return attr
       }
-    }))
+      shadow.innerHTML = describe.replace(Mustache, mapPropToAttr)
+    }
+  }))
 })()
